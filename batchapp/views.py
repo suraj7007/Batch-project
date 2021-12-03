f53d90ef9b45f5e01c7b504561313fc5d0c1dcfe
Suraj Mahamuni <68770481+suraj7007@users.noreply.github.com>
2021-12-03 14:02:22 +0530

    view created

--- batchapp/views.py
+++ batchapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import *
+from django.contrib.auth.models import User
+from rest_framework.response import Response
 
 # Create your views here.
+def createbatch(request):
+    return Response({"status":200, "msg":"Respose recieved"})
