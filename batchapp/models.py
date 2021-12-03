eb58d88bada786be3723098987ea0ceab49e9ce8
Suraj Mahamuni <68770481+suraj7007@users.noreply.github.com>
2021-12-03 13:33:23 +0530

    created an app, initialised some changes

---
+++ batchapp/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+# Create your models here.
+class Batch(models.Model):
+    batch_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    batch_name = models.CharField(max_length=20)
+    number_of_code = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.user}, {self.code}"
+
+class Codes(models.Model):
+    code_id = models.AutoField(primary_key=True)
+    code = models.CharField(max_length=10)
+    batch = models.ForeignKey(Batch, on_delete=models.CASCADE)
+
+class BatchSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Batch
+        fields = "__all__"
+
+class CodesSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Codes
+        fields = "__all__"
